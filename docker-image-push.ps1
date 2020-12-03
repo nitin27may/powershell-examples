e3eceed67f9ce44196c29ff5055f303cf553e6ff
nitin27may <nitin27may@gmail.com>
2020-12-03 17:29:54 -0500

    updated file

--- docker-image-push.ps1
+++ docker-image-push.ps1
@@ -1,12 +1,13 @@
 param (
   [string]$repository = "nitin27may/", #repository name, required for docker hub
-  [string]$imageName = "mean", # image name
+  [string]$imageName = "dockerdemo", # image name
   [string]$username = $(Read-Host "Input user name" ), # registry username
   [SecureString]$securedPassword = $(Read-Host "Enter a Password" -AsSecureString), # registry password
   [Parameter(Mandatory = $true, HelpMessage = "Image Tag.")]
   [string]$tag = "1.0.0", # variable for image tag
   [string]$dockerhost = "", # Container registry URL, if blank will try to connect docker hub
-  [string]$workspace = "D:\Workplace\mean-docker" # Source code where dockerfile is present 
+  [string]$workspace = "D:\Workplace\DockerDemo" # Source code where dockerfile is present 
+  # https://github.com/nitin27may/powershell-examples.git demo project
 )
 
 $password = ConvertFrom-SecureString -SecureString $securedPassword -AsPlainText
